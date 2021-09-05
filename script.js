5ac231eb21da1f329b3ffb9062067ae815bb630a
Sergei <nadoroge@gmail.com>
2021-09-05 18:15:28 +0300

    классы, массивы, передачи по ссылке

--- script.js
+++ script.js
@@ -1,35 +1,76 @@
 "use strict";
 
-function server(){
-    setTimeout(function(){
-        console.log(1);
-    },1000)
+//const obj = new Object();  //один из вариантов создания объектов
+
+//самый частый вариант создания объекта
+const option = {
+    name: 'test',
+    width: '640',
+    height: '480',
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+console.log(option.name);
+console.log(option['name']); //реже используемый вариант нотации
+
+//delete option.name; //удаление какого-либо свойства из объекта
+
+console.log(option);  
+
+//перебрать все элементы объекта
+for (let key in option){
+    console.log(`Свойство ${key} имеет значение ${option[key]}`);
 }
-function foo(){
-            console.log(2);
-   }
 
-server();
-foo();   
+//перебрать все элементы объекта
+let counter = 0;
+for (let key in option){
+    if (typeof(option[key]) === 'object') {
+        for (let i in option[key]){
+            console.log(`Свойство ${i} имеет значение ${option[key][i]}`); 
+            counter++;     
+        }
 
-function serverNew(host, callback){
-    console.log(`Server ${host} is starting...`);
-    callback();
+    }else{
+       console.log(`Свойство ${key} имеет значение ${option[key]}`); 
+       counter++;
+    }
 }
+//количество свойств в объекте
+console.log(counter);
 
-// serverNew('MyServer', function(){
-//     console.log('connect success!');
-// });
+console.log(Object.keys(option));
+console.log(Object.keys(option).length);
 
-function done(){
-    console.log('connect success!');
-}
+//добавим поведение для объекта
+const option2 = {
+    name: 'test',
+    width: '640',
+    height: '480',
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function(){  //метод объекта
+        console.log("Yes!");  
+    }
+};
 
-serverNew('MyServer', done);
+option2.makeTest();
 
-/*
-Задание 
-Реализовать свой пример call-back функции 
-сохранить в Github
+//деструктуризация объекта (вытаскиваем нужные нам свойства)
+const {border, bg} = option2.colors;
+console.log(border);
 
+
+/*Задание
+Создайте объект game
+в нем 7 свойств
+и для одного из свойств - сделайте свойство в виде объект, состоящего из трех свойств
+Добавьте два метода и вызовите их.
+С помощью деструктуризации выведете два любых свойства в консоль
 */
+
